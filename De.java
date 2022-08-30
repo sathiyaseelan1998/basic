6b5683b5ee78e741fc9583cec6263130d249d164
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-30 11:49:25 +0530

    array size changed

--- De.java
+++ De.java
@@ -2,13 +2,13 @@ public class De
 {
 	public static void main(String arg[])
 	{
-		int n=5;
+		int n=4;
 		int a[]=new int[n];
 		a[0]=10;
 		a[1]=20;
 		a[2]=30;
 		a[3]=40;
-		a[4]=50;
+		
 		for(int i=0;i<n;i++)
 		{
 			System.out.println(a[i]);
