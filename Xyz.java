a3975ea2d242779d01a839c92e13b01bee37f934
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-30 11:39:14 +0530

    add some new files

---
+++ Xyz.java
@@ -0,0 +1,19 @@
+public class Xyz
+{
+	int a,b;
+	Xyz()
+	{
+		int a=10;
+		this.a=a;
+
+	}
+
+	public static void main(String arg[])
+	{
+		Xyz ob=new Xyz();
+		
+		System.out.println(ob.a);
+
+	}
+
+}
