70f83f5d2a1ef522606b9dbacb7c04adeb43541f
sathiyaseelan1998 <109800614+sathiyaseelan1998@users.noreply.github.com>
2023-06-14 12:17:16 +0530

    mysql queries

--- sqlQuery.sql
+++ sqlQuery.sql
@@ -32,3 +32,44 @@ select * from employee where name like '%m';
 select * from employee where name like '___i%';
 
 select * from employee where salary between 30000 and 50000;
+
+--------------------------------------------------------------------------------------------------
+use basic;
+
+select * from employee;
+
+select * from branch;
+
+select * from employee where salary>=50000;
+
+select * from employee where salary in(25000,80000);
+
+select * from employee where salary not in(25000,80000);
+
+select * from employee where salary between 25000 and 70000;
+
+select name from employee where name like '__r%';
+
+select name from employee order by name desc;
+
+select job,count(job) from employee group by job;
+
+select job,count(job)as j from employee group by job having j=2;
+
+select branchId,count(branchId) from employee group by branchId;
+
+select e.name,e.salary,job,b.branch from employee as e join branch as b on e.branchId=b.branchId where branch='chennai';
+
+
+
+select * from employee where salary =(select max(salary) from employee);
+
+select * from employee where branchId =(select branchId from branch where branch='chennai');
+
+select b.branch,max(salary),count(empId) from employee as e right join branch as b on e.branchId=b.branchId group by b.branchId;
+
+select * from branch as b where branchId in (select branchId from employee as e where b.branchId=e.branchId);
+
+
+
+
