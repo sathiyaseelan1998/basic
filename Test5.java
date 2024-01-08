345e34bae48202a3ac56970264355beb2af5c7bc
sathiyaseelan1998 <109800614+sathiyaseelan1998@users.noreply.github.com>
2024-01-08 09:26:29 +0530

    java program

--- Test5.java
+++ Test5.java
@@ -16,7 +16,39 @@ public class Test5 {
 		int[] ar={1,2,3,4,5};
 		//t.moveNumber(ar);
 		//t.moveNTHTime();
-		t.a5b3c2();
+		//t.a5b3c2();
+		//t.addMatrix();
+		t.multipleMatrix();
+	}
+
+	private void multipleMatrix() {
+		// TODO Auto-generated method stub
+		int[][] a={{1,2,3},{1,2,3},{1,2,3}};
+		int[][] b={{1,2,3},{1,2,3},{1,2,3}};
+		int[][] c=new int[a.length][a[0].length];
+		for(int i=0;i<a.length;i++){
+			for(int j=0;j<a[i].length;j++){
+				c[i][j]=0;
+				for(int k=0;k<a[i].length;k++){
+					c[i][j]+=a[i][k]*b[k][j];
+				}
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private void addMatrix() {
+		// TODO Auto-generated method stub
+		int[][] a={{1,2,3},{1,2,3}};
+		int[][] b={{1,2,3},{1,2,3}};
+		int[][] c=new int[a.length][a[0].length];
+		for(int i=0;i<a.length;i++){
+			for(int j=0;j<a[i].length;j++){
+				System.out.print((c[i][j]=a[i][j]+b[i][j])+" ");
+			}
+			System.out.println();
+		}
 	}
 
 	private void a5b3c2() {
